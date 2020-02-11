2a9cf1d3e2b9c9987c8a08658ec1d2adeb91c4a1
Meawwat <meawwat@live.com>
2020-02-12 01:23:17 +0700

    add checkmark on persona hover

--- projects/inbox-lib/src/lib/mail/persona/persona.component.ts
+++ projects/inbox-lib/src/lib/mail/persona/persona.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'persona',
   templateUrl: './persona.component.html',
-  styleUrls: ['./persona.component.scss']
+  styleUrls: [
+    '../../../core.scss', 
+    '../../../../../../node_modules/office-ui-fabric-core/dist/css/fabric.min.css',
+    './persona.component.scss'
+  ]
 })
 export class PersonaComponent implements OnInit {
 
