3727db136231a5c06de4402983539d76082cd7d9
Meawwat <meawwat@live.com>
2020-02-11 21:47:21 +0700

    add persona component

--- projects/inbox-lib/src/lib/inbox-lib.module.ts
+++ projects/inbox-lib/src/lib/inbox-lib.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { InboxLibComponent } from './inbox-lib.component';
 import { MailComponent } from './mail/mail.component';
+import { PersonaComponent } from './mail/persona/persona.component';
 
 @NgModule({
-  declarations: [InboxLibComponent, MailComponent],
+  declarations: [InboxLibComponent, MailComponent, PersonaComponent],
   imports: [
   ],
   exports: [InboxLibComponent]
