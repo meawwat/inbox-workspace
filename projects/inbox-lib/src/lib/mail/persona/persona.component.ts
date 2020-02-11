02d953ec5a3c1ba9d98d14b71f17c9901c8b632b
Meawwat <meawwat@live.com>
2020-02-12 01:39:21 +0700

    add logic get initial from name & change color random by email address

--- projects/inbox-lib/src/lib/mail/persona/persona.component.ts
+++ projects/inbox-lib/src/lib/mail/persona/persona.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ColorCode } from './color-code.array';
 
 @Component({
   selector: 'persona',
@@ -10,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class PersonaComponent implements OnInit {
+  @Input() name: string;
+  @Input() email: string;
+  
+  initial:string = "";
+  color:string = ColorCode[0].code;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initial = this.getInitial();
+    this.color = this.getColor();
+  }
+  
+  getInitial(): string {
+    let arrName: string[];
+
+    if (this.name && this.name.length > 0) {
+      arrName = this.name.trim().split(' ');
+    } else return "";
+
+    if (arrName.length < 1 || arrName[0].length < 1) return "";
+    if (arrName.length === 1) {
+      return arrName[0].charAt(0);
+    }
+    return arrName[0].charAt(0) + arrName[arrName.length - 1].charAt(0);
   }
 
+  getColor(): string {
+    let sum = 0;
+    for (let i = 0; i < this.email.length; i++) {
+      sum += this.email.charCodeAt(i);
+    }
+    let random = sum % ColorCode.length;
+    return ColorCode[random].code;//same email will get same color
+  }
 }
