4fedd3a3ea0cc1c59134d65808fe84f491975b2c
Meawwat <meawwat@live.com>
2020-02-11 21:39:36 +0700

    add mail component

--- projects/inbox-lib/src/lib/inbox-lib.module.ts
+++ projects/inbox-lib/src/lib/inbox-lib.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { InboxLibComponent } from './inbox-lib.component';
-
-
+import { MailComponent } from './mail/mail.component';
 
 @NgModule({
-  declarations: [InboxLibComponent],
+  declarations: [InboxLibComponent, MailComponent],
   imports: [
   ],
   exports: [InboxLibComponent]
